1205c245345094102054b170c6cb2d63660ad0c2
gaoxuerong <1991403547@qq.com>
2018-10-31 22:51:33 +0800

    add city-router

---
+++ src/components/CityHead.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+     城市选择
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="stylus" scpoed>
+  @import '~@/assets/styles/varibles.styl'
+  .header
+    overflow hidden
+    height $headerHeight
+    line-height $headerHeight
+    text-align center
+    color #fff
+</style>
+
+
---
+++ src/views/City.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    给 i 哦几波 i 集
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'City',
+  components: {
+   
+  },
+  data () {
+    return {
+      
+    }
+  },
+
+  methods: {
+    
+  },
+  mounted () {
+    
+  },
+  
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
+
+
