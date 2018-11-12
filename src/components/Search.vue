663b50067077cc0263a2a2ac3663419f7a4158c5
gaoxuerong <1991403547@qq.com>
2018-11-12 14:41:01 +0800

    add city-search-logic

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -17,7 +17,7 @@
         >
           {{item.name}}
         </li>
-        <li class="search-item border-bottom">
+        <li class="search-item border-bottom" v-show="hasNoData">
           没有找到匹配数据
         </li>
       </ul>
@@ -25,6 +25,7 @@
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
   name: 'CitySearch',
   props: {
@@ -35,6 +36,37 @@ export default {
       keyword: '',
       list: [],
       timer: null
+    }
+  },
+  methods: {
+    handleCityClick (city) {
+      this.changeCity(city)
+      this.$router.push('/')
+    },
+  },
+  watch: {
+    keyword () {
+      
+      this.timer = setTimeout(() => {
+        const result = []
+        for(let i in this.cities){
+          console.log('i:')
+          this.cities[i].forEach(value => {
+            if(value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          });
+        }
+        this.list = result
+      },100)
+    }
+  },
+  mounted () {
+    let scroll = new BScroll(this.$refs.search)
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
     }
   }
 }
